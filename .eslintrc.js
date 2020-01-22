3babb79db3f854624ab0ace20c0e47af591e6daf
Yulya Vorontsova <secondnumber01@gmail.com>
2020-01-22 18:04:06 +0300

    build(components): add eslint
    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": ["airbnb", "prettier"],
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error",
+        "no-unused-vars": "warn",
+        "no-console": "off",
+        "func-names": "off",
+        "object-shorthand": "off",
+        "class-methods-use-this": "off"
+    }
+};
